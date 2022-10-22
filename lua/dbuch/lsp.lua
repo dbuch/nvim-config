7c39ed97e2efa9dc67812c0eb88fe63035a15ed9
Daniel Buch Hansen <boogiewasthere@gmail.com>
2022-10-22 17:52:45 +0200

    remove unused

--- lua/dbuch/lsp.lua
+++ lua/dbuch/lsp.lua
@@ -71,30 +71,7 @@ for type, icon in pairs(signs) do
     vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
 end
 
-
-
---[[ lsp_status.register_progress()
-lsp_status.config {
-  select_symbol = function(cursor_pos, symbol)
-    if symbol.valueRange then
-      local value_range = {
-        ["start"] = {
-          character = 0,
-          line = vim.fn.byte2line(symbol.valueRange[1])
-        },
-        ["end"] = {
-          character = 0,
-          line = vim.fn.byte2line(symbol.valueRange[2])
-        }
-      }
-
-      return require("lsp-status.util").in_range(cursor_pos, value_range)
-    end
-  end
-} ]]
-
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
 
 local servers = {
   html = {
