44ccd6e502e4a5fe4b7f5b0369d0610c0f7504f9
Daniel Buch Hansen <boogiewasthere@gmail.com>
2022-11-15 21:23:47 +0100

    pff

--- lua/dbuch/null-ls.lua
+++ lua/dbuch/null-ls.lua
@@ -1,9 +1,8 @@
 local null_ls = require("null-ls")
 null_ls.setup({
-  on_init = function(client, _)
-    vim.pretty_print(client)
-    client.offset_encoding = "utf-32"
-  end,
+  -- on_init = function(client, _)
+  --   client.offset_encoding = "utf-32"
+  -- end,
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.completion.spell,
