eb5a41e6ab75452522728b78ceb5c7d766c54988
Daniel Buch Hansen <boogiewasthere@gmail.com>
2022-11-27 15:57:16 +0100

    remove cmp_gh + cleanup null-ls

--- lua/dbuch/null-ls.lua
+++ lua/dbuch/null-ls.lua
@@ -1,8 +1,5 @@
 local null_ls = require("null-ls")
 null_ls.setup({
-  -- on_init = function(client, _)
-  --   client.offset_encoding = "utf-32"
-  -- end,
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.completion.spell,
