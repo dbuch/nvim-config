ad69899dafd389a1c317783346842e82c49238e3
Daniel Buch Hansen <boogiewasthere@gmail.com>
2023-02-11 22:47:28 +0100

    Cleanup make colorizer work

--- lua/dbuch/others.lua
+++ lua/dbuch/others.lua
@@ -1,3 +1,5 @@
+-- 'rafamadriz/friendly-snippets',
+
 -- {
 --   'ahmedkhalf/project.nvim',
 --   init = function()
@@ -35,190 +37,197 @@
 -- },
 
 return {
-    -- Init
-    {
-        'stevearc/dressing.nvim',
+  -- Init
+  {
+    'stevearc/dressing.nvim',
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require('lazy').load { plugins = { 'dressing.nvim' } }
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require('lazy').load { plugins = { 'dressing.nvim' } }
+        return vim.ui.input(...)
+      end
+    end,
+  },
+  {
+    'rcarriga/nvim-notify',
+    init = function()
+      require('lazy').load { plugins = { 'nvim-notify' } }
+    end,
+    config = function()
+      vim.notify = require 'notify'
+    end,
+  },
+  -- Editor
+  -- Dap
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'mfussenegger/nvim-dap-python',
+    },
+  },
+  { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap' } },
+  -- Buffer
+  {
+    dir = '~/dev/nvim/hover.nvim/',
+    keys = {
+      { 'K', '<Cmd>Hover<cr>' },
+      { 'gK', '<Cmd>HoverSelect<cr>' },
+    },
+    config = function()
+      vim.keymap.set('n', 'K', require('hover').hover, { desc = 'hover.nvim' })
+      vim.keymap.set('n', 'gK', require('hover').hover_select, { desc = 'hover.nvim (select)' })
+      require('hover').setup {
         init = function()
-          ---@diagnostic disable-next-line: duplicate-set-field
-          vim.ui.select = function(...)
-            require('lazy').load { plugins = { 'dressing.nvim' } }
-            return vim.ui.select(...)
-          end
-          ---@diagnostic disable-next-line: duplicate-set-field
-          vim.ui.input = function(...)
-            require('lazy').load { plugins = { 'dressing.nvim' } }
-            return vim.ui.input(...)
-          end
+          require 'hover.providers.lsp'
+          --require('hover.providers.gh')
+          require 'hover.providers.dictionary'
+          --require('hover.providers.diagnostic')
+          require 'hover.providers.man'
         end,
+      }
+    end,
+  },
+  {
+    'numToStr/Comment.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end,
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = function()
+      require('nvim-autopairs').setup {
+        check_ts = true,
+      }
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+      'nvim-treesitter/nvim-treesitter',
     },
-    {
-        'rcarriga/nvim-notify',
-        init = function()
-          require('lazy').load { plugins = { 'nvim-notify' } }
+  },
+  {
+    'AndrewRadev/bufferize.vim',
+    cmd = 'Bufferize',
+    config = function()
+      vim.g.bufferize_command = 'enew'
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'bufferize',
+        group = 'vimrc',
+        callback = function()
+          vim.opt_local.wrap = true
         end,
+      })
+    end,
+  },
+  -- Coloring
+  {
+    'lewis6991/nvim-colorizer.lua',
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = {
+      RGB = true,
+      RRGGBB = true,
+      names = true,
+      RRGGBBAA = false,
+      rgb_fn = false,
+      hsl_fn = false,
+      css = false,
+      css_fn = false,
+      mode = 'background', -- Set the display mode.
+    },
+    -- config = function()
+    --   require('colorizer').setup()
+    -- end,
+  },
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      'ray-x/lsp_signature.nvim',
+      'onsails/lspkind-nvim',
+      {
+        'j-hui/fidget.nvim',
         config = function()
-          vim.notify = require 'notify'
-        end,
-    },
-    -- Editor
-    -- Dap
-    {
-        'mfussenegger/nvim-dap',
-        dependencies = {
-            'mfussenegger/nvim-dap-python',
-        },
-    },
-    { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap' } },
-    -- Buffer
-    {
-        dir = '~/dev/nvim/hover.nvim/',
-        keys = {
-          { 'K', '<Cmd>Hover<cr>' },
-          { 'gK', '<Cmd>HoverSelect<cr>' },
-        },
-        config = function()
-          vim.keymap.set('n', 'K', require('hover').hover, { desc = 'hover.nvim' })
-          vim.keymap.set('n', 'gK', require('hover').hover_select, { desc = 'hover.nvim (select)' })
-          require('hover').setup {
-              init = function()
-                require 'hover.providers.lsp'
-                --require('hover.providers.gh')
-                require 'hover.providers.dictionary'
-                --require('hover.providers.diagnostic')
-                require 'hover.providers.man'
-              end,
-          }
-        end,
-    },
-    {
-        'numToStr/Comment.nvim',
-        event = { 'BufReadPre', 'BufNewFile' },
-        dependencies = { 'nvim-treesitter/nvim-treesitter' },
-        config = function()
-          require('Comment').setup {
-              pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-          }
-        end,
-    },
-    {
-        'windwp/nvim-autopairs',
-        event = 'InsertEnter',
-        config = function()
-          require('nvim-autopairs').setup {
-              check_ts = true,
-          }
-          local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-          local cmp = require 'cmp'
-          cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-        end,
-        dependencies = {
-            'hrsh7th/nvim-cmp',
-            'nvim-treesitter/nvim-treesitter',
-        },
-    },
-    {
-        'AndrewRadev/bufferize.vim',
-        cmd = 'Bufferize',
-        config = function()
-          vim.g.bufferize_command = 'enew'
-          vim.api.nvim_create_autocmd('FileType', {
-              pattern = 'bufferize',
-              group = 'vimrc',
-              callback = function()
-                vim.opt_local.wrap = true
-              end,
-          })
-        end,
-    },
-    -- Coloring
-    'folke/lsp-colors.nvim',
-    {
-        'lewis6991/nvim-colorizer.lua',
-        config = function()
-          require('colorizer').setup()
-        end,
-    },
-    -- Icons
-    'nvim-tree/nvim-web-devicons',
-    -- 'ryanoasis/vim-devicons',
-    -- LSP
-    {
-        'neovim/nvim-lspconfig',
-        event = { 'BufReadPre', 'BufNewFile' },
-        dependencies = {
-            'ray-x/lsp_signature.nvim',
-            'onsails/lspkind-nvim',
-            {
-                'j-hui/fidget.nvim',
-                config = function()
-                  require('fidget').setup {
-                      text = {
-                          spinner = 'dots',
-                      },
-                      fmt = {
-                          stack_upwards = false,
-                          task = function(task_name, message, percentage)
-                            local pct = percentage and string.format(' (%s%%)', percentage) or ''
-                            if task_name then
-                              return string.format('%s%s [%s]', message, pct, task_name)
-                            else
-                              return string.format('%s%s', message, pct)
-                            end
-                          end,
-                      },
-                      sources = {
-                          ['null-ls'] = {
-                              ignore = true,
-                          },
-                      },
-                  }
-                end,
+          require('fidget').setup {
+            text = {
+              spinner = 'dots',
             },
-        },
-        config = function()
-          require 'dbuch.lsp'
-        end,
-    },
-    -- 'rafamadriz/friendly-snippets',
-    {
-        'hrsh7th/nvim-cmp',
-        version = false,
-        event = 'InsertEnter',
-        dependencies = {
-            'L3MON4D3/LuaSnip',
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-nvim-lsp-signature-help',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-calc',
-            'hrsh7th/cmp-cmdline',
-            'hrsh7th/cmp-nvim-lua',
-            'f3fora/cmp-spell',
-            {
-                'saecki/crates.nvim',
-                event = 'BufRead Cargo.toml',
-                config = true,
+            fmt = {
+              stack_upwards = false,
+              task = function(task_name, message, percentage)
+                local pct = percentage and string.format(' (%s%%)', percentage) or ''
+                if task_name then
+                  return string.format('%s%s [%s]', message, pct, task_name)
+                else
+                  return string.format('%s%s', message, pct)
+                end
+              end,
             },
-        },
-        config = function()
-          require 'dbuch.cmp'
+            sources = {
+              ['null-ls'] = {
+                ignore = true,
+              },
+            },
+          }
         end,
+      },
     },
-    -- Treesitter
-    {
-        'nvim-treesitter/nvim-treesitter',
-        version = false,
-        build = ':TSUpdate',
-        event = { 'BufReadPost', 'BufNewFile' },
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter-context',
-            'nvim-treesitter/nvim-treesitter-refactor',
-            'nvim-treesitter/nvim-treesitter-textobjects',
-            'JoosepAlviste/nvim-ts-context-commentstring',
-            'theHamsta/nvim-dap-virtual-text',
-        },
-        config = function()
-          require 'dbuch.treesitter'
-        end,
+    config = function()
+      require 'dbuch.lsp'
+    end,
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    version = false,
+    event = 'InsertEnter',
+    dependencies = {
+      'L3MON4D3/LuaSnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-calc',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-nvim-lua',
+      'f3fora/cmp-spell',
+      {
+        'saecki/crates.nvim',
+        event = 'BufRead Cargo.toml',
+        config = true,
+      },
     },
+    config = function()
+      require 'dbuch.cmp'
+    end,
+  },
+  -- Treesitter
+  {
+    'nvim-treesitter/nvim-treesitter',
+    version = false,
+    build = ':TSUpdate',
+    event = { 'BufReadPost', 'BufNewFile' },
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-context',
+      'nvim-treesitter/nvim-treesitter-refactor',
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'JoosepAlviste/nvim-ts-context-commentstring',
+      'theHamsta/nvim-dap-virtual-text',
+    },
+    config = function()
+      require 'dbuch.treesitter'
+    end,
+  },
 }
