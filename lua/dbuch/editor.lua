5f8d2d427ab8e4799516176937e567dba0427b18
Daniel Buch Hansen <boogiewasthere@gmail.com>
2023-05-14 14:49:40 +0200

    Try projections.nvim for project management

--- lua/dbuch/editor.lua
+++ lua/dbuch/editor.lua
@@ -221,6 +221,32 @@ return {
     end,
   },
   {
+    'gnikdroy/projections.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'nvim-lua/telescope.nvim',
+    },
+    config = function()
+      require("projections").setup({
+        workspaces = {     -- Default workspaces to search for
+          -- { "~/Documents/dev", { ".git" } },        Documents/dev is a workspace. patterns = { ".git" }
+          -- { "~/repos", {} },                        An empty pattern list indicates that all subdirectories are considered projects
+          { "~/dev", { ".git", ".svn", ".hg" } },        -- dev is a workspace. default patterns is used (specified below)
+          { "~/dev/rust/", { ".git", ".svn", ".hg" } },        -- dev is a workspace. default patterns is used (specified below)
+          { "~/dev/nvim/plugins/", { ".git", ".svn", ".hg" } },        -- dev is a workspace. default patterns is used (specified below)
+        },
+        -- patterns = { ".git", ".svn", ".hg" },      -- Default patterns to use if none were specified. These are NOT regexps.
+        -- store_hooks = { pre = nil, post = nil },   -- pre and post hooks for store_session, callable | nil
+        -- restore_hooks = { pre = nil, post = nil }, -- pre and post hooks for restore_session, callable | nil
+        -- workspaces_file = "path/to/file",          -- Path to workspaces json file
+        -- sessions_directory = "path/to/dir",        -- Directory where sessions are stored
+      })
+      require('telescope').load_extension('projections')
+      vim.keymap.set("n", "<leader>p", function() vim.cmd("Telescope projections") end)
+    end
+
+  },
+  {
     -- TODO(Perhaps?): https://github.com/isaksamsten/nvim-config/blob/1500077595ac4848624c25f940f65706b1136813/lua/plugins/tasks.lua
     'stevearc/overseer.nvim',
     lazy = false,
