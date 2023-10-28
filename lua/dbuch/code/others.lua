7a88f3e0f528b77fe67b230a8e36d23b62a2635d
Daniel Buch Hansen <boogiewasthere@gmail.com>
2023-10-28 10:03:28 +0200

    more cleanup

--- lua/dbuch/code/others.lua
+++ lua/dbuch/code/others.lua
@@ -6,22 +6,6 @@ return {
       vim.g.matchup_matchparen_offscreen = { method = 'status_manual' }
     end,
   },
-  -- {
-  --   'windwp/nvim-autopairs',
-  --   event = { 'BufReadPost', 'BufNewFile' },
-  --   opts = {
-  --     check_ts = true,
-  --   },
-  --   config = function(_, opts)
-  --     require('nvim-autopairs').setup(opts)
-  --     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-  --     local cmp = require 'cmp'
-  --     cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-  --   end,
-  --   dependencies = {
-  --     'hrsh7th/nvim-cmp',
-  --   },
-  -- },
   {
     'LhKipp/nvim-nu',
     build = ':TSInstall nu',
