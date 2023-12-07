eefd56afc06818bd8b3bc3ec80b0d2d01f449a90
Daniel Buch Hansen <boogiewasthere@gmail.com>
2023-12-07 07:54:14 +0100

    Normalized_stdpath

--- lua/dbuch/env.lua
+++ lua/dbuch/env.lua
@@ -1,14 +1,35 @@
 M = {}
 
+---comment
+---@param what string
+---@return string|string[]|nil
+function Normalized_stdpath(what)
+  local path = vim.fn.stdpath(what)
+  if path == nil then
+    return nil
+  end
+
+  if type(path) == 'string' then
+    return vim.fs.normalize(path)
+  end
+
+  local paths = {}
+  for _, value in ipairs(path) do
+    path = vim.fs.normalize(value)
+    table.insert(paths, path)
+  end
+  return paths
+end
+
 ---@type boolean
 M.is_unix = vim.loop.os_uname().sysname:match 'Windows' == nil
----@type string
-M.cache_dir = vim.fs.normalize(vim.fn.stdpath 'cache')
----@type string
-M.data_dir = vim.fs.normalize(vim.fn.stdpath 'data')
----@type string
-M.log_dir = vim.fs.normalize(vim.fn.stdpath 'log')
----@type string
-M.undo_dir = vim.fs.normalize(M.cache_dir .. '/undodir/')
+---@type string|string[]|nil
+M.cache_dir = Normalized_stdpath 'cache'
+---@type string|string[]|nil
+M.data_dir = Normalized_stdpath 'data'
+---@type string|string[]|nil
+M.log_dir = Normalized_stdpath 'log'
+---@type string|string[]|nil
+M.undo_dir = Normalized_stdpath(M.cache_dir .. '/undodir/')
 
 return M
