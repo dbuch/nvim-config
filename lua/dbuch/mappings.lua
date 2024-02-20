a9d4210f1b6ecf5c3bae9a29f1c64894f45267b9
Daniel Buch Hansen <boogiewasthere@gmail.com>
2024-02-20 20:46:21 +0100

    update keymaps

--- lua/dbuch/mappings.lua
+++ lua/dbuch/mappings.lua
@@ -28,19 +28,17 @@ map('n', '<leader>g', ':Telescope live_grep<CR>', { nowait = true, silent = true
 map('n', '<leader>e', ':lua MiniFiles.open()<CR>', { silent = true })
 map('n', '<c-q>', ':bd<CR>', { silent = true })
 
-map('n', 'ga', vim.lsp.buf.code_action, { silent = true })
-map('n', 'gD', ':Telescope lsp_definitions<CR>', { silent = true })
-map('n', 'gd', vim.lsp.buf.definition, { silent = true })
-map('n', 'gn', vim.lsp.buf.rename, { silent = true })
-map('n', 'gt', ':TroubleToggle<CR>', { silent = true })
-map('n', 'gr', ':TroubleToggle lsp_references<CR>', { silent = true })
-map('n', 'gi', function()
+map('n', 'ca', vim.lsp.buf.code_action, { silent = true })
+map('n', 'cD', ':Telescope lsp_definitions<CR>', { silent = true })
+map('n', 'cd', vim.lsp.buf.definition, { silent = true })
+map('n', 'cn', vim.lsp.buf.rename, { silent = true })
+map('n', 'ct', ':TroubleToggle<CR>', { silent = true })
+map('n', 'cr', ':TroubleToggle lsp_references<CR>', { silent = true })
+map('n', 'ci', function()
   require('dbuch.traits.nvim').inlay_hint_toggle()
 end, { silent = true })
-
-map('n', '<leader>w', '<esc>:w<CR>', { noremap = false })
-
 map('n', 'K', vim.lsp.buf.hover, { desc = 'hover.nvim' })
+map('n', '<leader>w', '<esc>:w<CR>', { noremap = false })
 
 -- Clear search
 map('n', '<esc>', ':noh<return><esc>', { silent = true })
