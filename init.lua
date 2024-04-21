c24324058c772606bbbde9eea40b5c6985eba5f2
Daniel Buch Hansen <boogiewasthere@gmail.com>
2024-04-21 10:20:10 +0200

    fidget + statuscol update

--- init.lua
+++ init.lua
@@ -19,14 +19,16 @@ vim.g.loaded_ruby_provider = 0
 ---@type string
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
+  vim
+    .system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      lazypath,
+    })
+    :wait()
 end
 
 vim.opt.rtp:prepend(lazypath)
--- lua/dbuch/plugins/ui.lua
+++ lua/dbuch/plugins/ui.lua
@@ -14,14 +14,52 @@ return {
     branch = '0.10',
     event = 'VeryLazy',
     -- lazy = false,
-    opts = {
-      setopt = true,
-    },
+    config = function(_, _)
+      local builtin = require 'statuscol.builtin'
+      local lnum_func = function(args)
+        if args.rnu ~= 0 and not args.nu then
+          return ''
+        end
+
+        if args.virtnum ~= 0 then
+          return '%='
+        end
+
+        local highlight = ((args.relnum % 10 > 0) and '%#CurrentLineNr#' or '%#Normal#')
+
+        if args.relnum == 0 then
+          return highlight .. '%l%='
+        else
+          return highlight .. '%=%r'
+        end
+      end
+
+      require('statuscol').setup {
+        setopt = true,
+        segments = {
+          { text = { '%C' }, click = 'v:lua.ScFa' },
+          { text = { '%s' }, click = 'v:lua.ScSa' },
+          {
+            text = { lnum_func, ' ' },
+            condition = { true, builtin.not_empty },
+            click = 'v:lua.ScLa',
+          },
+        },
+      }
+    end,
   },
   {
     'j-hui/fidget.nvim',
     event = 'VeryLazy',
     opts = {
+      progress = {
+        poll_rate = 400,
+        ignore_done_already = true,
+        ignore_empty_message = true,
+        lsp = {
+          progress_ringbuf_size = 2048,
+        },
+      },
       notification = {
         override_vim_notify = true,
         window = {
