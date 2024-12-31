4c7df39ebca69ea7dd8b0eaa715fbcb3524e686a
Daniel Buch Hansen <boogiewasthere@gmail.com>
2024-12-31 10:15:21 +0100

    ...

--- lua/dbuch/plugins/editor.lua
+++ lua/dbuch/plugins/editor.lua
@@ -168,7 +168,7 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
-    ft = { 'markdown' },
+    ft = 'markdown',
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {
